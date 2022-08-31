828f77a0ad6a85a9f5c580f3837cb98798d7430e
caspar <824394795@qq.com>
2022-08-31 16:35:03 +0800

    添加创建存证，删除存证，转移存证逻辑及对应配置  20220831

--- substrate-node-template/runtime/src/lib.rs
+++ substrate-node-template/runtime/src/lib.rs
@@ -266,6 +266,11 @@ impl pallet_template::Config for Runtime {
 	type Event = Event;
 }
 
+impl pallet_poe::Config for Runtime {
+	type Event = Event;
+	type MaxClaimLength =ConstU32<512>;
+}
+
 // Create the runtime by composing the FRAME pallets that were previously configured.
 construct_runtime!(
 	pub enum Runtime where
@@ -283,6 +288,7 @@ construct_runtime!(
 		Sudo: pallet_sudo,
 		// Include the custom logic from the pallet-template in the runtime.
 		TemplateModule: pallet_template,
+		PoeModule: pallet_poe,
 	}
 );
 
